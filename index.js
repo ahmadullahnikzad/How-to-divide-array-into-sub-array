a2bf4d31e6e1fdcd2b7eb23168689fa4bc307ebf
ahmadullah <ahmadullahnikzad1919@gmail.com>
2020-09-20 13:19:16 +0000

    solution 3 added

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ function makeSubArray(arr,size){
   return newArray;
 }
 makeSubArray([1,2,3,4,5,6,7,8,9],3);
+//solutiion 2
+function makeSubArray2(arr,size){
+  let subArrays=[];
+  let index=0;
+  for(index;index<arr.length;index+=size){
+subArrays.push(arr.slice(index,index+size));
+  }
+  return subArrays;
+}
+makeSubArray2([1,2,3,4,5,5,6,7,8,9,10],3);
+//solutiion 3
+function createSubArray3(arr,size){
+  let newArr=[];
+  while(arr.length){
+    newArr.push(arr.splice(0,size))
+  }
+  return newArr;
+}
+createSubArray3([1,2,3,4,5,5,6,7,7,8,'a','b'],3);
